2f6684db6917e0e4a1c15b4026302af15e8698b3
Dentalmovil R4 <117413675+Dentalmovil@users.noreply.github.com>
2026-02-22 17:08:09 -0500

    Crear src/constants/data.ts

---
+++ src/constants/data.ts
@@ -0,0 +1,12 @@
+export const GANADERIA_DATA = {
+  nacional: {
+    pib: "1.4%",
+    empleos: "1.1 Millones",
+    inventario: "30.3M cabezas"
+  },
+  cesar: {
+    inventario: "1.7M cabezas",
+    pib_agro: "50%+",
+    principales_municipios: ["Valledupar", "Aguachica", "Codazzi", "El Paso"]
+  }
+}
